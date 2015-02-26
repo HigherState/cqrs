8063557cd1367a1b26f805a47c5f72b2e6ae7490
Jamie Pullar <jpullar@gmail.com>
2015-02-26 16:56:42 +0000

    Actor Selection

--- src/main/scala/org/higherState/cqrs/adapters/ActorDispatcherFactory.scala
+++ src/main/scala/org/higherState/cqrs/adapters/ActorDispatcherFactory.scala
@@ -3,7 +3,7 @@ package org.higherState.cqrs.adapters
 import org.higherState.cqrs._
 import scala.concurrent.Future
 import scala.reflect.ClassTag
-import akka.actor.ActorRef
+import akka.actor.{ActorSelection, ActorRef}
 import akka.util.Timeout
 import org.higherState.cqrs.std.{Valid, FutureValid}
 
@@ -12,6 +12,14 @@ object ActorDispatcherFactory {
   import akka.pattern.ask
 
   def future[M <: Message](messageReceiver:ActorRef)(implicit timeout:Timeout) =
+    new Dispatcher[Future, M] {
+      def sendMessage[T: ClassTag](m: => M): Future[T] =
+        messageReceiver
+          .ask(m)
+          .mapTo[T]
+    }
+
+  def future[M <: Message](messageReceiver:ActorSelection)(implicit timeout:Timeout) =
     new Dispatcher[Future, M] {
       def sendMessage[T: ClassTag](m: => M): Future[T] =
         messageReceiver
@@ -33,6 +41,20 @@ object ActorDispatcherFactory {
           .mapTo[T]
     }
 
+  def future[C <: Command, QP <: QueryParameters](commandHandler:ActorSelection, queryExecutor:ActorSelection)(implicit timeout:Timeout) =
+    new CQDispatcher[Future, C, QP] {
+
+      def sendCommand(c: => C): Future[Unit] =
+        commandHandler
+          .ask(c)
+          .mapTo[Unit]
+
+      def executeQuery[T: ClassTag](qp: => QP): Future[T] =
+        queryExecutor
+          .ask(qp)
+          .mapTo[T]
+    }
+
   def futureValid[M <: Message, E](messageReceiver:ActorRef)(implicit timeout:Timeout) =
     new Dispatcher[({type V[+T] = FutureValid[E,T]})#V, M] {
       def sendMessage[T: ClassTag](m: => M): FutureValid[E, T] =
@@ -41,7 +63,29 @@ object ActorDispatcherFactory {
           .mapTo[Valid[E, T]]
     }
 
+  def futureValid[M <: Message, E](messageReceiver:ActorSelection)(implicit timeout:Timeout) =
+    new Dispatcher[({type V[+T] = FutureValid[E,T]})#V, M] {
+      def sendMessage[T: ClassTag](m: => M): FutureValid[E, T] =
+        messageReceiver
+          .ask(m)
+          .mapTo[Valid[E, T]]
+    }
+
+
   def futureValid[C <: Command, QP <: QueryParameters, E](commandHandler:ActorRef, queryExecutor:ActorRef)(implicit timeout:Timeout) =
+    new CQDispatcher[({type V[+T] = FutureValid[E,T]})#V, C, QP] {
+      def sendCommand(c: => C): FutureValid[E, Unit] =
+        commandHandler
+          .ask(c)
+          .mapTo[Valid[E, Unit]]
+
+      def executeQuery[T: ClassTag](qp: => QP): FutureValid[E, T] =
+        queryExecutor
+          .ask(qp)
+          .mapTo[Valid[E, T]]
+    }
+
+  def futureValid[C <: Command, QP <: QueryParameters, E](commandHandler:ActorSelection, queryExecutor:ActorSelection)(implicit timeout:Timeout) =
     new CQDispatcher[({type V[+T] = FutureValid[E,T]})#V, C, QP] {
       def sendCommand(c: => C): FutureValid[E, Unit] =
         commandHandler
